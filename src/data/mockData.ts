dab954559bf08108033e88b708a98c5845cc723f
Hrishabh Gupta <162270765+hrishabh1103@users.noreply.github.com>
2025-12-04 10:52:21 +0530

    Remove unnecessary comment from mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -16,7 +16,7 @@ export interface Property {
   hostName: string;
   hostImage: string;
 }
-
+// can u please update the whole app soon
 export interface Destination {
   name: string;
   state: string;
