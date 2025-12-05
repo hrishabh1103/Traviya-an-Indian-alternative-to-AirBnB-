3df309320c313c7d19b344b1b84314ae2a376595
Hrishabh Gupta <162270765+hrishabh1103@users.noreply.github.com>
2025-12-05 14:14:19 +0530

    Update Hero.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -5,7 +5,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
+// cxhanfe the main text over heer
 interface User {
   id: string;
   email: string;
